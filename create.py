bac5100d03d0f567997a11b8120bdbf1d67a10ec
Adjoshi06 <adjoshi.06@gmail.com>
2020-10-18 17:37:42 +0530

    Added sentiment model

--- create.py
+++ create.py
@@ -1,18 +1,18 @@
 import csv
 import sys
 csv.field_size_limit(sys.maxsize)
-with open('./extract_data.csv', 'w') as csvreader:
+with open('./extract_data.csv', 'w',encoding='utf8') as csvreader:
     writer = csv.writer(csvreader, delimiter=',')
     writer.writerow(["id", "content"])
-    with open('./articles1.csv','r') as csvwriter:
+    with open('./articles1.csv','r',encoding='utf8') as csvwriter:
         reader = csv.DictReader(csvwriter)
         for row in reader:
             writer.writerow([row['id'], row['content']])
-    with open('./articles2.csv','r') as csvwriter:
+    with open('./articles2.csv','r',encoding='utf8') as csvwriter:
         reader = csv.DictReader(csvwriter)
         for row in reader:
 	           writer.writerow([row['id'], row['content']])
-    with open('./articles3.csv','r') as csvwriter:
+    with open('./articles3.csv','r',encoding='utf8') as csvwriter:
 	       reader = csv.DictReader(csvwriter)
 	       for row in reader:
 	              writer.writerow([row['id'], row['content']])
